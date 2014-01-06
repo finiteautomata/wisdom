4a1aeb766e38327a162271ef8f8ae32047ff0890
jmperez <jmperez.85@gmail.com>
2014-01-06 00:14:50 -0300

    Tide up code.

--- wisdom/lesk.py
+++ wisdom/lesk.py
@@ -6,26 +6,32 @@ from itertools import chain
 
 ps = PorterStemmer()
 
+def __build_dictionary(synset, hyperhypo):
+    lesk_dictionary = []
+
+    # Includes definition.
+    lesk_dictionary+= synset.definition.split()
+    # Includes lemma_names.
+    lesk_dictionary+= synset.lemma_names
+    # Optional: includes lemma_names of hypernyms and hyponyms.
+    if hyperhypo:
+        related_senses = synset.hypernyms()+synset.hyponyms()
+        lesk_dictionary+= [lemma.name for related_synset in related_senses for lemma in related_synset.lemmas ]       
+
+    return lesk_dictionary
+
+
+
 def lesk(context_sentence, ambiguous_word, pos=None, stem=True, hyperhypo=True):
     max_overlaps = 0; lesk_sense = None
     context_sentence = context_sentence.split()
     for ss in wn.synsets(ambiguous_word):
-        # If POS is specified.
-        if pos and ss.pos is not pos:
+        if pos and ss.pos != pos:
             continue
 
-        lesk_dictionary = []
-
-        # Includes definition.
-        lesk_dictionary+= ss.definition.split()
-        # Includes lemma_names.
-        lesk_dictionary+= ss.lemma_names
-
-        # Optional: includes lemma_names of hypernyms and hyponyms.
-        if hyperhypo == True:
-            lesk_dictionary+= list(chain(*[i.lemma_names for i in ss.hypernyms()+ss.hyponyms()]))       
-
-        if stem == True: # Matching exact words causes sparsity, so lets match stems.
+        lesk_dictionary = __build_dictionary(synset=ss, hyperhypo=hyperhypo)
+        # Matching exact words causes sparsity, so lets match stems.
+        if stem: 
             lesk_dictionary = [ps.stem(i) for i in lesk_dictionary]
             context_sentence = [ps.stem(i) for i in context_sentence] 
 
